54059ce07204df2ec8743884da0dc3a9827ca95b
Viet <viet-anh.le@hotmail.co.uk>
2020-03-20 00:27:10 +0000

    Admin Calendar init

---
+++ src/components/AdminCalendar/AdminCalendar.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Line from '../Line/Line';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const AdminCalendar: React.FunctionComponent = (props: any) => {
+  const localizer = momentLocalizer(moment);
+  const now = new Date();
+  const events = [
+    {
+      id: 15,
+      title: '1Treatment',
+      start: now,
+      end: now,
+    },
+    {
+      id: 16,
+      title: '2Treatment',
+      start: now,
+      end: now,
+    },
+    {
+      id: 17,
+      title: '3Treatment',
+      start: now,
+      end: now,
+    },
+  ];
+  return (
+    <div style={{ height: '800px', width: '1000px', fontSize: '10px' }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        defaultDate={moment().toDate()}
+        defaultView="day"
+        dayLayoutAlgorithm="overlap"
+        // timeslots={2}
+        timeslots={5}
+        // toolbar={false}
+        formats={{ dateFormat: 'D', weekdayFormat: 'dddd' }}
+        // showMultiDayTimes={true}
+        min={
+          new Date(
+            new Date().getFullYear(),
+            new Date().getMonth(),
+            new Date().getDate(),
+            9
+          )
+        }
+        max={
+          new Date(
+            new Date().getFullYear(),
+            new Date().getMonth(),
+            new Date().getDate(),
+            18
+          )
+        }
+        // step={15}
+        step={12}
+      />
+    </div>
+  );
+};
+
+export default AdminCalendar;
